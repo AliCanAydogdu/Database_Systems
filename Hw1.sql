45e674999347c27b207aaadfcfa898fc27f41f4f
Ali Can Aydogdu <alicantx@gmail.com>
2017-03-07 15:09:14 -0600

    Update Hw1.sql

--- Hw1.sql
+++ Hw1.sql
@@ -1,5 +1,5 @@
 #Ali Can Aydogdu
-Based on Sakila Database
+#Based on Sakila Database
 
 --Write a Sql query that brings all information from CITY table about Cities which Country ID is 15.
 
@@ -9,14 +9,14 @@ CITY
 where
 country_id = 15;
 
---	How many City are there in the CITY table? (Only SQL query)
+--How many City are there in the CITY table? (Only SQL query)
 
 
 select count(*) AS NUMBEROFCITIES
 from
 CITY;
 
---	Write a Sql query that brings all information from CITY table which City Name starts with ‘A’ or ‘B’
+--Write a Sql query that brings all information from CITY table which City Name starts with ‘A’ or ‘B’
 
 
 select*
@@ -25,21 +25,21 @@ CITY
 where
 city like “A%” OR city like “B%”;
 
---	Write a Sql query that brings City and CityId info from CITY table.
+--Write a Sql query that brings City and CityId info from CITY table.
 
 
 select city, city_id
 from
 CITY;
 
---	Write a Sql query that brings FirstName, LastName, CustomerID  info from CUSTOMER table
+--Write a Sql query that brings FirstName, LastName, CustomerID  info from CUSTOMER table
 
 
 select first_name, last_name, customer_id
 from
 CUSTOMER;
 
---	Write a Sql query that brings all  info from CUSTOMER table whose name is not ‘Jennifer’
+--Write a Sql query that brings all  info from CUSTOMER table whose name is not ‘Jennifer’
 
 
 select *
@@ -48,7 +48,7 @@ CUSTOMER
 where
 first_name NOT IN(“Jennifer”);
 
---	Write a Sql query that brings all information from PAYMENT table which amount is between 100 and 150.
+--Write a Sql query that brings all information from PAYMENT table which amount is between 100 and 150.
 
 
 select*
@@ -57,7 +57,7 @@ Payment
 where
 amount between 1.00 and 1.50;
 
---	Write a Sql query that brings all information from PAYMENT table which amount is bigger than 150
+--Write a Sql query that brings all information from PAYMENT table which amount is bigger than 150
 
 
 select *
@@ -66,14 +66,14 @@ PAYMENT
 where
 amount >1.50;
 
---	Write a Sql query that brings First_name from ACTOR table. Eliminate same names.
+--Write a Sql query that brings First_name from ACTOR table. Eliminate same names.
 
 
 select distinct first_name
 from
 ACTOR;
 
---	Write a Sql query that brings all information from PAYMENT table which belongs to “CINDY FISHER” * you can find info about “CINDY FISHER” in CUSTOMER table.
+--Write a Sql query that brings all information from PAYMENT table which belongs to “CINDY FISHER” * you can find info about “CINDY FISHER” in CUSTOMER table.
 
 
 select *
